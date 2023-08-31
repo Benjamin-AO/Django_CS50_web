c87529d60fe253360b9d52e44933fd98fdde5f89
Benjamin <benabi.officials@gmail.com>
2023-08-31 16:29:57 +0100

    completed lecture 3 todo list app

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('newyear/', include("newyear.urls"))
+    path('tasks/', include("tasks.urls")),
 ]
